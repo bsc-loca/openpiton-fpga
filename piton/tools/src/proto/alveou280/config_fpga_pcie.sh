910448042e560376ff3099e4e4f96799d6c118e5
Alexander Kropotov <alexander.kropotov@bsc.es>
2021-05-21 13:06:44 +0200

    Adding basic scripts to configure FPGA, PCIe, run bare-metal app, boot linux.

---
+++ piton/tools/src/proto/alveou280/config_fpga_pcie.sh
@@ -0,0 +1,26 @@
+
+#To build basic Ariane design:
+#  $ cd $PITON_ROOT/
+#  $ source piton/ariane_setup.sh # Piton tools setup (https://github.com/PrincetonUniversity/openpiton#environment-setup-1)
+#  $ protosyn --board alveou280 --design system --core ariane --x_tiles 1 --y_tiles 1 --uart-dmw ddr --zeroer_off
+
+script=${BASH_SOURCE[0]}
+if [ $script == $0 ]; then
+    echo "ERROR: You must source this script"
+    exit 2
+fi
+
+#Load PCIe bitstream to FPGA and setup host PCIe environment
+source /home/tools/scripts/load-bitstream.sh qdma ../meep_openpiton/build/alveou280/system/alveou280_system/alveou280_system.runs/impl_1/system.bit
+
+#Some sanity checks
+# dma-ctl qdma08000 reg dump
+# dma-ctl qdma08001 reg dump
+# dma-ctl qdma08002 reg dump
+# dma-ctl qdma08003 reg dump
+lspci -vd 10ee:
+ls /dev/qdma* -al
+dmesg | grep tty
+
+#Applying both resets to the design
+dma-ctl qdma08000 reg write bar 2 0x0 0x0
