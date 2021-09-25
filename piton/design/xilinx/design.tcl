7f35a6bc47fc5b18ea3bf5d9fdb44b09bfdb2728
Daniel J.Mazure <daniel.jimenez2@bsc.es>
2021-09-25 15:01:10 +0200

    Added some other files to the project building process. Some of them could be removed, but I'm delaying that fine graning for later

--- piton/design/xilinx/design.tcl
+++ piton/design/xilinx/design.tcl
@@ -45,6 +45,7 @@ if {$::env(PITON_ARIANE) != "0"} {
 
 if {$::env(PITON_LAGARTO) != "0" } {
     set CORE_RTL_FILES [concat \
+	${OTHER_RTL_FILES} \
 	${LAGARTO_RTL_FILES} \
 	${MEEP_VPU_FILES} \
     ]
