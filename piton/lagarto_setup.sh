14816038c4ac06dbac3ec663ca37b94acfe05efb
Ivan Vera <ivan.vera@bsc.es>
2021-11-09 16:23:46 +0100

    Modified to run isa test from vas_tile_core repo

--- piton/lagarto_setup.sh
+++ piton/lagarto_setup.sh
@@ -72,13 +72,13 @@ fi
 # set root directory
 export PITON_ROOT=`pwd`
 export LAGARTO_ROOT=${PITON_ROOT}/piton/design/chip/tile/vas_tile_core/
-export ARIANE_ROOT=${PITON_ROOT}/piton/design/chip/tile/ariane/
 
 ## GCC and RISCV GCC setup
 export CXX=g++ CC=gcc
 # customize this to a fast local disk
-export RISCV=/scratch/`whoami`/riscv_install
-export VERILATOR_ROOT=$ARIANE_ROOT/tmp/verilator-4.014/
+export RISCV=/home/tools/openpiton/riscv_install
+#export RISCV=/home/`whoami`/tools/riscv_install
+export VERILATOR_ROOT=$LAGARTO_ROOT/tmp/verilator-4.014/
 
 # setup paths
 export PATH=$RISCV/bin:$VERILATOR_ROOT/bin:$PATH
@@ -92,7 +92,6 @@ export CPLUS_INCLUDE_PATH=$RISCV/include:$VERILATOR_ROOT/include
 source ./piton/piton_settings.bash
 
 export RISCV=/home/tools/openpiton/riscv_install
-export VERILATOR_ROOT=/home/tools/openpiton/open-piton/piton/design/chip/tile/ariane/tmp/verilator-4.014/
 export PATH=$RISCV/bin:$VERILATOR_ROOT/bin:$PATH
 export LIBRARY_PATH=$RISCV/lib
 export LD_LIBRARY_PATH=$RISCV/lib
@@ -105,7 +104,7 @@ then
     echo
     echo "----------------------------------------------------------------------"
     echo "setup complete. do not forget to run the following script             "
-    echo "if you run the setup for the first time: ./piton/ariane_build_tools.sh"
+    echo "if you run the setup for the first time: ./piton/lagarto_build_tools.sh"
     echo "----------------------------------------------------------------------"
     echo
 else
