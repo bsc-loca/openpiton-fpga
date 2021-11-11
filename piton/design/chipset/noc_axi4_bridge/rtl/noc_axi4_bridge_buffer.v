271a154049eae0789f7ac7d68bdec4335c376f94
Alexander Kropotov <alexander.kropotov@bsc.es>
2021-11-11 19:36:10 +0100

    Bug fix: proper enabling of new captured response ID (excluding previous ID usage).

--- piton/design/chipset/noc_axi4_bridge/rtl/noc_axi4_bridge_buffer.v
+++ piton/design/chipset/noc_axi4_bridge/rtl/noc_axi4_bridge_buffer.v
@@ -201,12 +201,20 @@ always @(posedge clk)
 (* keep="TRUE" *) (* mark_debug="TRUE" *) wire write_resp_val_act = write_resp_val && (RDWR_REORDER || ( outstnd_command[write_resp_id] && outstnd_abs_rdptrs_val[write_resp_id]));
 wire [NUM_REQ_THREADS_LOG2 : 0] full_rd_resp_id = {RDWR_REORDER ? READ :1'b0, read_resp_id };
 wire [NUM_REQ_THREADS_LOG2 : 0] full_wr_resp_id = {RDWR_REORDER ? WRITE:1'b0, write_resp_id};
+(* keep="TRUE" *) (* mark_debug="TRUE" *) reg resp_val;
 always @(posedge clk)
-  if(~rst_n || init_outstnd_mem) full_resp_id <= {(NUM_REQ_THREADS_LOG2+1){1'b0}};
+  if(~rst_n || init_outstnd_mem) begin 
+    full_resp_id <= {(NUM_REQ_THREADS_LOG2+1){1'b0}};
+    resp_val <= 1'b0;
+  end
   else if (outstnd_vrt_empt || outstnd_abs_rdptr_val) begin
     // higher priority for Read response in case the coming ID is not which we already have 
     if (write_resp_val_act && !(read_resp_val_act  && full_resp_id == full_rd_resp_id)) full_resp_id <= full_wr_resp_id;
     if (read_resp_val_act  && !(write_resp_val_act && full_resp_id == full_wr_resp_id)) full_resp_id <= full_rd_resp_id;
+
+    if (write_resp_val_act ||
+        read_resp_val_act) resp_val <= 1'b1;
+    if (ser_go)            resp_val <= 1'b0;
   end
 
 
@@ -315,16 +323,7 @@ xilinx_true_dual_port_write_first_1_clock_ram #(
     .doutb(clean_header)              // Port B RAM output data, width determined from RAM_WIDTH
 );
 
-(* keep="TRUE" *) (* mark_debug="TRUE" *) reg resp_val;
-always @(posedge clk)
-  if(~rst_n) resp_val <= 1'b0;
-  else begin
-    if (write_resp_val_act |
-        read_resp_val_act) resp_val <= 1'b1;
-    if (ser_go)            resp_val <= 1'b0;
-  end
 (* keep="TRUE" *) (* mark_debug="TRUE" *) wire outstnd_abs_rdptr_val_act = outstnd_abs_rdptr_val & resp_val;
-
 (* keep="TRUE" *) (* mark_debug="TRUE" *) reg stor_hdr_val;
 always @(posedge clk)
   if(~rst_n) stor_hdr_val <= 1'b0;
