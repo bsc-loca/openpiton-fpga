8741c9bbe49d35d188fed7d420a33f73402df470
Daniel J.Mazure <daniel.jimenez2@bsc.es>
2022-02-18 06:30:03 +0100

    minor fix

--- piton/design/chipset/meep_shell/accelerator_build.sh
+++ piton/design/chipset/meep_shell/accelerator_build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-ROOT_DIR=$(PWD)
 ACC_DIR=$1
-
-cd $ACC_DIR
 
 
 #Use this script to call protosyn
@@ -11,7 +8,6 @@ cd $ACC_DIR
 CORE=lagarto
 
 source $ACC_DIR/piton/${CORE}_settings.sh
-make protosyn CORE=$CORE XTILES=2 YTILES=2 PROTO_OPTIONS="--meep --vpu --eth"
+make protosyn CORE=$CORE XTILES=2 YTILES=2 PROTO_OPTIONS="--meep --vpu --eth --hbm"
 
-cd $ROOT_DIR
 
