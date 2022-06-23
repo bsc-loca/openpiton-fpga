27ab42dcf388acbd70ecc27f3f602d3bcd486cfe
Daniel J.Mazure <daniel.jimenez2@bsc.es>
2022-06-24 00:51:50 +0200

    Bug: The signals need to have the last semi-colon separated one space. This needs to be fixed

--- piton/design/chipset/meep_shell/accelerator_mod.sv
+++ piton/design/chipset/meep_shell/accelerator_mod.sv
@@ -64,8 +64,8 @@ module openpiton_wrapper (
     input mem_calib_complete,
 
     // Ethernet
-    input             eth_axi_aclk,
-    input             eth_axi_arstn,	    
+    input             eth_axi_aclk ,
+    input             eth_axi_arstn ,	    
 
     // AXI interface
     output  [5:0]     eth_axi_awid ,
