4c91ae376d90763081ca22ae0b713044cc2aa5df
Daniel J.Mazure <daniel.jimenez2@bsc.es>
2022-07-27 15:34:57 +0200

    Update VAS

--- piton/design/chipset/meep_shell/accelerator_build.sh
+++ piton/design/chipset/meep_shell/accelerator_build.sh
@@ -2,7 +2,7 @@
 
 #Use this script to call protosyn
 
-CORE=ariane
+CORE=lagarto
 
-make protosyn CORE=$CORE XTILES=1 YTILES=1 PROTO_OPTIONS="--meep --eth --hbm"
+make protosyn CORE=$CORE XTILES=1 YTILES=1 PROTO_OPTIONS="--meep --eth --hbm --vpu"
 
