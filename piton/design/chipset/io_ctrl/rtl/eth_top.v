1c8d7d22d47811989fe0764da8d13da6f5bfec21
Alexander Kropotov <alexander.kropotov@bsc.es>
2022-10-01 00:25:38 +0200

    Simulation fix for eth_top.v when no option -eth is applied in both protosyn or sims.

--- piton/design/chipset/io_ctrl/rtl/eth_top.v
+++ piton/design/chipset/io_ctrl/rtl/eth_top.v
@@ -84,9 +84,9 @@ module eth_top #(
     output  [3:0]  qsfp_4x_gtx_p
     `else     
 	 // in PITONSYS_MEEP the CLK, RST, and interrupts are inputs to this core
-     input                 				  net_axi_clk,
-	 input                 			      net_axi_arstn,	 	 	 
-	 input  [NUM_INTR-1:0] 	     	      net_axi_intr,
+     input                                net_axi_clk,
+     input                                net_axi_arstn,	 	 	 
+     input  [NUM_INTR-1:0]                net_axi_intr,
 	 
      output [`AXI4_ID_WIDTH     -1:0]     core_axi_awid,
      output [`AXI4_ADDR_WIDTH   -1:0]     core_axi_awaddr,
@@ -141,6 +141,13 @@ module eth_top #(
     `endif
 `endif // PITON_FPGA_ETH_CMAC
 );
+
+`ifndef PITONSYS_MEEP
+	wire net_axi_clk;
+	wire net_axi_arstn;
+	wire [NUM_INTR-1:0]  net_axi_intr; // Needs to be CDCd before output
+`endif
+
 
 `ifdef PITON_FPGA_ETH
 
@@ -238,13 +245,6 @@ wire  [`AXI4_USER_WIDTH   -1:0]    core_axi_buser;
 wire                               core_axi_bvalid;
 wire                               core_axi_bready;
  `endif
-`endif
-
-`ifndef PITONSYS_MEEP
-	wire net_axi_clk;
-	wire net_axi_arstn;
-	wire [NUM_INTR-1:0]  net_axi_intr; // Needs to be CDCd before output
-
 `endif
 
 
@@ -595,6 +595,8 @@ assign core_axi_buser  = `AXI4_USER_WIDTH'h0;
     assign net_phy_tx_en        = 1'b0;
     assign net_phy_mdc          = 1'b0;
 
+    assign net_axi_intr = {NUM_INTR{1'h0}};
+
 `endif  // PITON_FPGA_ETH
 
 // CDC the interrupts that are in the net_axi_clk domain to the chipset_clk before output them
@@ -614,8 +616,6 @@ generate
    
   end
  endgenerate    
-
-
 
 
 endmodule
