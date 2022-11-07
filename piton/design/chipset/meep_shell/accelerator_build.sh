10997e1f5321cd17fb5f20f222b26706bfbbc851
Daniel J.Mazure <daniel.jimenez2@bsc.es>
2022-11-07 20:51:22 +0100

    #TestCICD generate a Matrix for all the different configurations

--- piton/design/chipset/meep_shell/accelerator_build.sh
+++ piton/design/chipset/meep_shell/accelerator_build.sh
@@ -5,12 +5,17 @@
 
 
 EA_MOD=$1
+MEEP=$2
+
+if [ $MEEP != "" ]; then
+    MEEP="--meep "
+fi
 
 # Default options
 CORE=lagarto
 XTILES=1
 YTILES=1
-PROTO_OPTIONS="--meep --eth --hbm --ncmem"
+PROTO_OPTIONS="--eth --hbm --ncmem"
 #PROTO_OPTIONS="--meep --eth --hbm --vpu"
 
 case "$EA_MOD" in
@@ -18,19 +23,21 @@ acme)
     CORE=lagarto
     echo "Selected build configuration: Lagarto 1x1"
     ;;
-openpiton)
+ariane)
     CORE=ariane
     echo "Selected build configuration: Ariane 1x1"
     ;;
 pronoc)
     CORE=lagarto
-    PROTO_OPTIONS="--meep --eth --hbm --ncmem --pronoc"
-    echo "Selected build configuration: Lagarto 1x1 with Pronoc"
+    XTILES=2
+    YTILES=2
+    PROTO_OPTIONS="--eth --hbm --ncmem --pronoc"
+    echo "Selected build configuration: Lagarto 2x2 with Pronoc"
     ;; 
 meep_dvino)
     CORE=lagarto
-    PROTO_OPTIONS="--meep --eth --hbm --ncmem --pronoc --vpu"
-    echo "Selected build configuration: MEEP DVINO with Pronoc"
+    PROTO_OPTIONS="--eth --hbm --vpu"
+    echo "Selected build configuration: MEEP DVINO"
     ;;
 acme_v2)
     CORE=lagarto
@@ -43,10 +50,12 @@ acme_vpu)
     XTILES=2
     YTILES=2
     # Add VPU
-    PROTO_OPTIONS="--meep --eth --hbm --vpu --pronoc"
+    PROTO_OPTIONS="--eth --hbm --vpu --pronoc"
     echo "Selected build configuration: Lagarto 2x2 plus VPU and pronoc"
     ;;
 esac
+
+PROTO_OPTIONS=$MEEP$PROTO_OPTIONS
 
 make protosyn CORE=$CORE XTILES=$XTILES YTILES=$YTILES PROTO_OPTIONS="$PROTO_OPTIONS"
 
