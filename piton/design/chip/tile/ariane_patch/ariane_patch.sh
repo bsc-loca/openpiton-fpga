b0f6597d05509079765b644044578c3ec1c7ff13
Alexander Kropotov <alexander.kropotov@bsc.es>
2023-10-16 00:01:36 +0200

    Update to official Ariane to commit `575cb4` (26/08/20) with patches for it.

---
+++ piton/design/chip/tile/ariane_patch/ariane_patch.sh
@@ -0,0 +1,5 @@
+# Modified by Barcelona Supercomputing Center on October, 2023
+
+echo "Patching Ariane submodule..."
+cp $DV_ROOT/design/chip/tile/ariane_patch/Flist.ariane $ARIANE_ROOT
+cp $DV_ROOT/design/chip/tile/ariane_patch/frontend.sv  $ARIANE_ROOT/src/frontend
